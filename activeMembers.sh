37f6712374aa4c686d6e8f143f0c3a6db3234624
Michael Vessia <michaelvessia@mail.adelphi.edu>
2015-08-18 08:51:54 -0400

    made corrections suggested by shellcheck.net

--- activeMembers.sh
+++ activeMembers.sh
@@ -12,10 +12,10 @@ if [ $# != 1 ]; then
 	exit
 fi
 # put all of the content of the csv files into one file
-cat *.csv > temp
+cat ./*.csv > temp
 
 # grep for lines containing emails, and put that into another file
-grep -o [A-Za-z]*@mail.adelphi.edu temp > temp2
+grep -o '[A-Za-z]*@mail.adelphi.edu' temp > temp2
 
 #remove the email extension, so we are just left with names
 sed 's/@mail.adelphi.edu//g' temp2 > temp3
@@ -31,9 +31,9 @@ tr '[:upper:]' '[:lower:]' < temp3 > temp4
 
 
 # put active members in a file and output results to console.
-echo The Active Members for $1 are currently:
+echo The Active Members for "$1" are currently:
 cat activeMembers.txt
-var=$(cat activeMembers.txt | wc -l)
-echo The number of active members for $1 are: $var
+var=$(wc -l < activemembers.txt)
+echo The number of active members for "$1" are: "$var"
 
 rm temp*
